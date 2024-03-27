6d5340713c9d223888a69caa7808ff7b2b5ff5dd
porchsal <porchsal@yahoo.com>
2024-03-27 14:48:00 -0400

    Lusitana deleted

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children } : { children: React.ReactNode}) {
   return (
     <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
         <div className='w-full flex-none md:w-64'>
-            <SideNav /> 
+            <SideNav />
 
         </div>
 
